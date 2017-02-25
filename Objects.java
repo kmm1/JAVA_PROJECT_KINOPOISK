0fa8b383e0bbbf671b0300f7a4df4f5cf2528ea8
kmm1 <km.kathrin3@gmail.com>
2017-02-26 02:39:15 +0300

    coment 1

---
+++ Objects.java
@@ -0,0 +1,14 @@
+/**
+ * Created by icons on 26.2.17.
+ */
+public class Objects {
+    public static void main(String[] args) {
+        Point pointA = new Point (5, 5);
+        pointA.distance();
+        Rectangle rectangle = new Rectangle (5, 6, 7, 8);
+        rectangle.diagonalLength();
+        rectangle.rectangleArea();
+
+    }
+}
+
